44272889d5a1862c040a8a9ed168f8ae9c865311
Shawnykim <40187093+Shawnykim@users.noreply.github.com>
2020-02-25 09:00:29 +0900

    Updates

---
+++ precache-manifest.f86f25d34afc3d6b9d4a604cb3855f79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1e33046b60075517794",
+    "url": "/test-portfolio/static/css/main.793b3b18.chunk.css"
+  },
+  {
+    "revision": "b1e33046b60075517794",
+    "url": "/test-portfolio/static/js/main.b1e33046.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/test-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "fe9a43f482cc3b6cdd13",
+    "url": "/test-portfolio/static/js/runtime~main.fe9a43f4.js"
+  },
+  {
+    "revision": "8e8fce15f88a9ab9b8e8110fd05462b5",
+    "url": "/test-portfolio/index.html"
+  }
+];
